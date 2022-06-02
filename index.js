6707e8cea597b4aecaf1683ad7102e4c51742d8b
Sagar Bisht <itssagar11@gmail.com>
2022-06-02 07:44:36 +0530

    Initial Commit

---
+++ index.js
@@ -0,0 +1,44 @@
+const express= require("express");
+const  connect  = require("http2");
+const app= express();
+const PORT= 8000;
+
+const httpServer= require('http').createServer(app);
+const io= require("socket.io")(httpServer);
+app.use(express.static("public"));
+let connection=[]
+io.on('connection',(socket)=>{
+    connection.push(socket);
+    console.warn(`${socket.id} has connected`);
+    socket.on('draw',(data)=>{
+        connection.forEach((con)=>{
+            if(con.id!=socket.id){
+                con.emit('ondraw',({x:data.x,y:data.y}));
+            }
+        })
+    });
+
+    socket.on('down',(data)=>{
+        connection.forEach((con)=>{
+            if(con.id!=socket.id){
+                con.emit('ondown',({x:data.x,y:data.y}));
+            }
+        })
+    });
+
+
+    socket.on('disconnect',(reason)=>{
+        console.warn(`${socket.id} has disconnected`);
+        connection=connection.filter((con)=>con.id!==socket.id);
+    })
+})
+
+
+
+
+
+
+
+httpServer.listen(8000,()=>{
+    console.warn(`Server Started on ${PORT}`);
+})
