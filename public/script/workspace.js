fad694106ac878e235bd66f245b4bf9c548fa93c
Sagar Bisht <itssagar11@gmail.com>
2022-07-14 20:34:54 +0530

    UI/UX update

---
+++ public/script/workspace.js
@@ -0,0 +1,13 @@
+function PenMenuClose(){
+document.getElementById("pen-sub-menu").style.display="none";
+}
+function PenMenuOpen(){
+    document.getElementById("pen-sub-menu").style.display="inline-block";
+    
+}
+function openLink(){
+    document.getElementById("link").style.display="inline-block";
+}
+function closelink(){
+    document.getElementById("link").style.display="none";
+}
